73be3c333964dc7e1ddeb0f4f62f342a2fd8182e
mkshimod <mkshimod@hawaii.edu>
2016-10-04 11:59:00 -1000

    Added bluetooth menu.

--- app/src/main/java/com/example/michele/pups_android_application/MainActivity.java
+++ app/src/main/java/com/example/michele/pups_android_application/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.michele.pups_android_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.bluetooth.*;
 
 public class MainActivity extends AppCompatActivity {
 
