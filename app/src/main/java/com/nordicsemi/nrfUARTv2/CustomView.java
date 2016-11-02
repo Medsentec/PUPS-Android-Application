463b2ea7f695a0af21f070335bb5ff627b486f4d
mkshimod <mkshimod@hawaii.edu>
2016-11-02 13:11:59 -1000

    New class for adding in grid

---
+++ app/src/main/java/com/nordicsemi/nrfUARTv2/CustomView.java
@@ -0,0 +1,71 @@
+package com.nordicsemi.nrfUARTv2;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Michele on 11/2/16.
+ */
+
+public class CustomView extends View {
+    private Rect rectangle;
+    private Paint paint;
+    private Integer color;
+
+    private Rect[][] myGrid = new Rect[10][10];
+    private Paint[][] myGridPaint = new Paint[10][10];
+    private Integer[][] myColors = new Integer[10][10];
+
+
+    public CustomView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        int x = 50;
+        int y = 50;
+        int sideLength = 100;
+
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                myColors[i][j] = Color.WHITE;
+                myGridPaint[i][j] = new Paint();
+                myGrid[i][j] = new Rect(x, y, sideLength, sideLength);
+                x += 100;
+            }
+            x = 50;
+            y += 100;
+        }
+
+
+
+        // create a rectangle that we'll draw later
+        // rectangle = new Rect(x, y, sideLength, sideLength);
+        // create the Paint and set its color
+//        paint = new Paint();
+//        color = Color.GRAY;
+
+    }
+
+
+    public void changeColor(int newColor) {
+        Paint newPaint = new Paint();
+        newPaint.setColor(newColor);
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.BLUE);
+        for(int i = 0; i < 10; i++) {
+            for(int j = 0; j < 10; j++) {
+                myGridPaint[i][j].setColor(myColors[i][j]);
+                canvas.drawRect(myGrid[i][j], myGridPaint[i][j]);
+            }
+        }
+
+    }
+}
+
